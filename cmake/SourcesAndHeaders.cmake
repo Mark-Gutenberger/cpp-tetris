c0caf70cb238307936b7bee0d909dc5a0dc31d77
Marc Gutenberger <gutenfries@gmail.com>
2023-07-31 15:26:05 -0500

    delete unit tests

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,4 +1,12 @@
-file(GLOB_RECURSE exe_sources "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
-file(GLOB_RECURSE headers "${CMAKE_CURRENT_SOURCE_DIR}/src/*.hpp")
-set(public_link_targets ncurses)
-set(private_link_targets "")
+file(GLOB_RECURSE
+	exe_sources "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp"
+)
+file(GLOB_RECURSE
+	headers "${CMAKE_CURRENT_SOURCE_DIR}/src/*.hpp"
+)
+file(GLOB_RECURSE
+	test_sources "tests/*.cpp"
+)
+
+set(public_link_targets "")
+set(private_link_targets ncurses)
