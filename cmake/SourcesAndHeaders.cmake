75b6a5e986393d92fdf1b22081b6fc0859f62a4d
Marc Gutenberger <gutenfries@gmail.com>
2023-07-31 13:29:42 -0500

    upgrade to Cmake

---
+++ cmake/SourcesAndHeaders.cmake
@@ -0,0 +1,4 @@
+file(GLOB_RECURSE exe_sources "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
+file(GLOB_RECURSE headers "${CMAKE_CURRENT_SOURCE_DIR}/src/*.hpp")
+set(public_link_targets ncurses)
+set(private_link_targets "")
